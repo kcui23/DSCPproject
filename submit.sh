66a1d76fef3227fa22016b482d806e627fac7c7e
kcui23 <kcui@foxmail.com>
2023-11-06 12:08:16 -0600

    rivision for parallel

---
+++ submit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf slurm_out;mkdir -p slurm_out
+rm -rf data;mkdir -p data
+
+sbatch --output="slurm_out/slurm-%A_%a.out" \
+       --error="slurm_out/slurm-%A_%a.err" \
+       --array 1-6000  getData.sh

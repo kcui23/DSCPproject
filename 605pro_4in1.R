8d71ed293d2cd9ddd6ad02133bc3b925e6336376
tchen465 <147094751+tchen465@users.noreply.github.com>
2023-12-03 15:31:07 -0600

    Update 605pro_4in1.R
    only 2 file outputs

--- 605pro_4in1.R
+++ 605pro_4in1.R
@@ -6,21 +6,19 @@ library(reshape)
 library(tidytext)
 
 #########
-#使用此代码时文件目录下应有NRC-Lexicon.txt和4个文件夹info,vocab,complex,senti用于存储文件
+#使用此代码时文件目录下应有NRC-Lexicon.txt和2个文件夹vocab,total用于存储文件
 
-#输出3个带header的csv，一个txt, csv第一列均为Name,以“10001-8.txt”为例,第一列为数值10001
+#输出1个带header的csv，一个txt, csv第一列为Name,以“10001-8.txt”为例,第一列为数值10001
 #包括以下文件：
-#作者/书名/年份 的info.csv
+#作者/书名/年份,/词汇复杂度/句子长度/F-K Grade/ G-F Index/ 情感占比  的total.csv
 #100最高频词及其占比的vocab.txt
-#词汇复杂度，句子长度，F-K Grade和 G-F Index的complex.csv
-#情感占比的sentiment.csv
+
 
 #####
 #情感词典使用NRC Word-Emotion Association Lexicon
 #https://raw.githubusercontent.com/kcui23/stat605_final_project/main/NRC-Lexicon.txt
 #使用该词典需要引用：！！！！！
 #Saif M. Mohammad and Peter Turney. (2013), ``Crowdsourcing a Word-Emotion Association Lexicon.'' Computational Intelligence, 29(3): 436-465.
-
 
 
 ##########非常重要！！！！
@@ -39,10 +37,11 @@ truedir = "./"
 # 处理路径，准备输入和输出的全部路径，输出默认在运行目录下的info,vocab,complex,senti文件夹
 namedir = paste(truedir,nameinput,sep = "")
 namenumber <- substr(nameinput, 1, 5)
-infoname <- paste("./info/", namenumber, "info.csv", sep = "")
-vocabname <- paste("./vocab/",namenumber, "vocab.txt", sep = "")
-gradename <- paste("./complex/",namenumber, "complex.csv", sep = "")
-sentiname <- paste("./senti/", namenumber, "sentiment.csv", sep = "")
+vocabname <- paste("./vocab/",namenumber, "vocab.csv", sep = "")
+totalname <- paste("./total/", namenumber, "total.csv", sep = "")
+
+# 获取停词
+stop_words <- stopwords("en")
 
 # 读取文本数据
 text <- readLines(namedir)
@@ -52,19 +51,30 @@ text <- readLines(namedir)
 # 查找并提取Author和Release Year
 author <- NA
 year <- NA
-if (any(str_detect(text, "Author:"))) {
-  author <- str_extract(text[str_detect(text, "Author:")], "Author: (.+)") %>% str_replace("Author: ", "")
+language <- NA
+# 查找并提取Author
+author_line_index <- which(str_detect(text, "Author:"))[1]
+if (length(author_line_index) > 0) {
+  author_line <- text[author_line_index]
+  author <- str_extract(author_line, "Author: ([^\r\n]+)") %>% str_replace_all("Author: ", "") %>% str_trim()
 }
+
+# 查找并提取Release Year
 if (any(str_detect(text, "Release Date:"))) {
   release_date_full <- str_extract(text[str_detect(text, "Release Date:")], "Release Date: .+\\[(.+)\\]") %>% str_replace("Release Date: ", "")
-  release_date_full <- gsub("EBook #[0-9]+", "", release_date_full)
   year <- as.numeric(str_extract(release_date_full, "\\b(\\d{4})\\b"))
 }
-info_df <- data.frame(namenumber, author, year)
-info_df[is.na(info_df)] <- ""
-# 将数据保存为csv文件
-write.table(info_df, infoname, sep = ",", row.names = FALSE, col.names = c("Name","Author","Year"), quote = FALSE)
 
+# 查找并提取Language
+if (any(str_detect(text, "Language:"))) {
+  language_line <- text[str_detect(text, "Language:")]
+  language <- str_extract(language_line, "Language: ([^\\n]+)") %>% str_replace("Language: ", "") %>% str_trim()
+}
+
+# 创建输出数据框
+info_df <- data.frame(namenumber, author, year, language)
+colnames(info_df) <- c("Name", "Author", "Year", "Language")
+info_df[is.na(info_df)] <- ""
 
 ##### 阅读难度与可读性
 
@@ -74,19 +84,20 @@ corpus <- tm_map(corpus, content_transformer(tolower))
 corpus <- tm_map(corpus, removePunctuation)
 corpus <- tm_map(corpus, removeNumbers)
 corpus <- tm_map(corpus, stripWhitespace)
+corpus <- tm_map(corpus, removeWords, stop_words)
 dtm <- DocumentTermMatrix(corpus)
 mat <- as.matrix(dtm)
 word.freq <- rowSums(mat)
 word.freq.col <- colSums(mat) 
 word.count <- sum(word.freq)
 
-### 高频词汇
 # 获取频率最高的100个词并计算占比
 top.words <- sort(word.freq.col, decreasing = TRUE)[1:100]
 word.proportion <- round(100 * top.words / word.count, 4)
-# 将数据保存为txt文件
+
+# 将数据保存为csv文件
 output.df <- data.frame(Word = names(top.words), Frequency = top.words, Proportion = word.proportion)
-write.table(output.df, vocabname, sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+write.csv(output.df, vocabname, row.names = FALSE, quote = FALSE)
 
 ### 基础数据
 # 词汇复杂度计算
@@ -109,9 +120,6 @@ gf.index <- 0.4 * ((word.count / sentence.count) + 100 * (complex.words.count / 
 # 将结果整理成dataframe
 results_df <- t(data.frame(c(fk.grade, gf.index, vocab.richness, avg.sentence.length)))
 colnames(results_df) <- c("F-KGrade", "G-FIndex", "VocabularyRichness", "SentenceLength")
-# 写入CSV文件
-results_df <- cbind(Name = namenumber, results_df)
-write.csv(results_df, gradename, row.names = FALSE, quote = FALSE)
 
 #####情感分析
 
@@ -129,6 +137,8 @@ emotion_freq <- text_df %>%
   summarise_all(sum, na.rm = TRUE)
 total_freq <- sum(emotion_freq)
 emotion_freq <- round(100 * emotion_freq / total_freq, 2)
+
 # 将数据保存为CSV文件
-emotion_freq <- cbind(Name = namenumber, emotion_freq)
-write.table(emotion_freq, sentiname, sep = ",",row.names = FALSE, quote = FALSE)
+total_df <- cbind(info_df, results_df)
+total_df <- cbind(total_df, emotion_freq)
+write.table(total_df, totalname, sep = ",",row.names = FALSE, quote = FALSE)
